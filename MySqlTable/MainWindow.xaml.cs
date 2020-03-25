c2d5c763807cfc3958901fd86653d430bcc4ffa2
Dima <bairachniy@gmail.com>
2020-03-25 19:21:14 +0200

    Add project files.

---
+++ MySqlTable/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MySqlTable
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MyTableProgram pr = new MyTableProgram();
+
+        
+            
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            pr.InsertQutry();
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UsersAppSql/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace UsersAppSql
+{
+    class Program
+    {
+        SqlConnection conn = null;
+
+        public Program()
+        {
+
+            conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=usersdb; Integrated Security=SSPI;");
+        }
+        static void Main(string[] args)
+        {
+            Program pr = new Program();
+
+            //pr.InsertQutry("insert into Users (Name, Age) values ('Lubov','18')");
+            pr.ReadData("select * from Users where Users.Age>18");
+        }
+        public void InsertQutry(string s)
+        {
+           
+            try
+            {
+                conn.Open();
+
+                string insertString1 = @""+s;
+                SqlCommand cmd = new SqlCommand(insertString1, conn);
+                cmd.ExecuteNonQuery();
+               
+
+            }
+            finally
+            {
+                if (conn != null)
+                    conn.Close();
+            }
+        }
+        public void ReadData(string s)
+        {
+            SqlDataReader rdr = null;
+            try
+            {
+                conn.Open();
+
+                string insertString1 = @""+s;
+                SqlCommand cmd = new SqlCommand(insertString1, conn);
+                rdr = cmd.ExecuteReader();
+                int line = 0;
+
+                while (rdr.Read())
+                {
+
+                    if (line == 0)
+                        for (int i = 0; i < rdr.FieldCount; i++)
+                        {
+                            Console.WriteLine(rdr.GetName(i).ToString() + " ");
+                        }
+                    Console.WriteLine();
+                    line++;
+                    Console.WriteLine(rdr[1] + " " + rdr[2]);
+                }
+                Console.WriteLine("Обработано записей: " + line.ToString());
+            }
+            finally
+            {
+                if (rdr != null)
+                    rdr.Close();
+                if (conn != null)
+                    conn.Close();
+
+            }
+        }
+    }
+}
