537b8a0494c9bfb91308faa2e69c7b0986a3a8c3
Dima <bairachniy@gmail.com>
2020-03-26 17:06:53 +0200

    Done

--- MySqlTable/MainWindow.xaml.cs
+++ MySqlTable/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MySqlTable
 {
@@ -20,26 +22,63 @@ namespace MySqlTable
     /// </summary>
     public partial class MainWindow : Window
     {
-        MyTableProgram pr = new MyTableProgram();
 
+        DataSet userStore = null;
+        DataTable userTable = null;
+        private SqlConnection connection=null;
+        SqlCommandBuilder cmd = null;
+        SqlDataAdapter sda = null;
         
-            
         public MainWindow()
         {
             InitializeComponent();
+            connection= new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=usersdb; Integrated Security=SSPI;");
+            label.Content = "UsersTable";
+        }  
+        private void FillData(string s)
+        {
+            try
+            {
+                userStore = new DataSet();
+                sda = new SqlDataAdapter(s, connection);
+                dataGrid.DataContext = null;
+                cmd = new SqlCommandBuilder(sda);
+                sda.Fill(userStore, "UsersTable");
+                dataGrid.DataContext = userStore.Tables["UsersTable"];
+            }
+            catch(Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+        private void Uptade()
+        {
+            sda.Update(userStore, "UsersTable");
         }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-
-
-            pr.InsertQutry();
-
+            FillData(textBox.Text);
         }
 
+       
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-
+            Uptade();
         }
+        public void Fill()
+        {
+            DataRow row = userTable.NewRow();
+            row.ItemArray = new object[] { null, "Sveta", "Malahitova" }; // добавляем первую строку
+            userTable.Rows.Add(row);// добавляем вторую строку
+            userTable.Rows.Add(new object[] { null, "Vlada", "Taratinkina" });
+            userTable.Rows.Add(new object[] { null, "Vika", "Korobova" });
+            userTable.Rows.Add(new object[] { null, "Kolya", "Tatushkina" });
+            userTable.Rows.Add(new object[] { null, "Decl", "Malaya" });
+            dataGrid.DataContext = userTable.DefaultView;
+        }
+
     }
 }
--- MySqlTable/MyTableProgram.cs
+++ MySqlTable/MyTableProgram.cs
@@ -9,67 +9,7 @@ namespace MySqlTable
 {
     class MyTableProgram
     {
-        SqlConnection conn = null;
-
-        public MyTableProgram()
-        {
-
-            conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=usersdb; Integrated Security=SSPI;");
-        }
-        public void InsertQutry(string s)
-        {
-
-            try
-            {
-                conn.Open();
-
-                string insertString1 = @"" + s;
-                SqlCommand cmd = new SqlCommand(insertString1, conn);
-                cmd.ExecuteNonQuery();
-
-
-            }
-            finally
-            {
-                if (conn != null)
-                    conn.Close();
-            }
-        }
-        public void ReadData(string s)
-        {
-            SqlDataReader rdr = null;
-            try
-            {
-                conn.Open();
-
-                string insertString1 = @"" + s;
-                SqlCommand cmd = new SqlCommand(insertString1, conn);
-                rdr = cmd.ExecuteReader();
-                int line = 0;
-
-                while (rdr.Read())
-                {
-
-                    if (line == 0)
-                        for (int i = 0; i < rdr.FieldCount; i++)
-                        {
-                            Console.WriteLine(rdr.GetName(i).ToString() + " ");
-                        }
-                    Console.WriteLine();
-                    line++;
-                    Console.WriteLine(rdr[1] + " " + rdr[2]);
-                }
-                Console.WriteLine("Обработано записей: " + line.ToString());
-            }
-            finally
-            {
-                if (rdr != null)
-                    rdr.Close();
-                if (conn != null)
-                    conn.Close();
-
-            }
-        }
+       
 
     }
 }
